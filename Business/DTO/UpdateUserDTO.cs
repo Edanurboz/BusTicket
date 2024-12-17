6bc152342d50c77587554a18dbb459977f45b0fa
Edanurboz <98693688+Edanurboz@users.noreply.github.com>
2024-12-17 23:15:07 +0300

    ses

--- Business/DTO/UpdateUserDTO.cs
+++ Business/DTO/UpdateUserDTO.cs
@@ -8,13 +8,17 @@ namespace Business.DTO
 {
     public class UpdateUserDTO
     {
-        public int user_id { get; set; }
-        public string name { get; set; }
-        public string surname { get; set; }
         public string email { get; set; }
-        public string password { get; set; }
-        public string phone_number { get; set; }
         public string gender { get; set; }
         public string identity_ { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public string phone_number { get; set; }
+        public string surname { get; set; }
+        public int user_id { get; set; }
+
+
+
+
     }
 }
